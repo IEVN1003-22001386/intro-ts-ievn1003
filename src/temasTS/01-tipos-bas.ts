3576f5e58496445b65eb757ba8b44829fa0e2486
Diana Torres <dianatj08@gmail.com>
2025-09-05 18:53:28 -0600

    Segundo commit 05/09/25

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,18 @@
+console.log("Hola mundo")
+
+var num1=23;
+var num2=32;
+const num3=67;
+
+let nombre:string
+let num:number
+let activo: boolean
+
+let sum = num1+num2+num3;
+console.log("la suma es: "+sum);
+
+let matricula:number|string
+
+matricula=22001386
+matricula='SYUKMHL'
+console.log(matricula)
