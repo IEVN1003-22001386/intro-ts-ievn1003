ea79d322c86799adc81d6f3b635d0b1d02b8fd86
Diana Torres <dianatj08@gmail.com>
2025-09-10 17:59:46 -0600

    FinDeTypeScript 10-09-25

---
+++ src/temasTS/06-importar-exportar.ts
@@ -0,0 +1,12 @@
+import { type Producto, calcularISV2 } from "./05-desestructura-funciones";
+
+const carrito:Producto[]=[
+    {nombre: 'Iphone 14', precio: 1500},
+    {nombre: 'Ipad Air', precio: 1200},
+    {nombre:'Macbook Pro', precio: 300}
+];
+
+const [total,isv]=calcularISV2(carrito);
+console.log('Total:',total);
+console.log('ISV:',isv);
+
